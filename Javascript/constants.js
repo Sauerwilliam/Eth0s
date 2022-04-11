22ce5dcbb2b82443623d9d61084b776dfd79dcfa
Sauerwilliam <69807222+Sauerwilliam@users.noreply.github.com>
2022-04-11 11:13:28 -0600

    Add files via upload

---
+++ Javascript/constants.js
@@ -0,0 +1,17 @@
+const prompts = [
+  ["hi", "hey", "how are you", "is anyone there", "hello", "good day"],
+  ["thanks", "thank you", "that's helpful", "thank's a lot!"],
+  ["What is ethos", "what do you do at ethos", "what does ethos do", "ethos","eth0s"],
+  ["help"],
+]
+const replies = [
+  ["Hey", "Hello, thanks for visiting", "Hi there, what can I do for you?", "Hi there, how can I help?"],
+  ["Happy to help!", "Any time!", "My pleasure"],
+  ["Eth0s is dedicated to preforming ethical hacking with their own devices!", "Eth0s is an ethical hacking group dedicated to unlocking their devices. For more info, visit our mission statement."],
+  ["Try typing 'ethos' to get started!"],
+]
+const alternative = [
+  "Try again",
+  "I don't understand :/",
+  "type help for more info"
+]
